9777687e736a7b25ca9de8f3200a06d67f69b4ab
Stepan <sters@ua.fm>
2021-02-08 17:39:02 +0200

    Initial commit

--- src/main/java/ua/com/tracktor/combine/CombineApplication.java
+++ src/main/java/ua/com/tracktor/combine/CombineApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CombineApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CombineApplication.class, args);
 	}
