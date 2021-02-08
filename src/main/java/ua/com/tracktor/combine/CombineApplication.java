5a4de0037258b65f42fb65b86506a19702bb7dea
Stepan <sters@ua.fm>
2021-02-08 17:39:02 +0200

    Initial commit

---
+++ src/main/java/ua/com/tracktor/combine/CombineApplication.java
@@ -0,0 +1,13 @@
+package ua.com.tracktor.combine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CombineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CombineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/com/tracktor/combine/CombineApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.tracktor.combine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CombineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
