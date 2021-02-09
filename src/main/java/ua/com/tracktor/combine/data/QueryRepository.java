475ef2277f492fbe3bf97711356e615ed9f3dce4
Stepan <sters@ua.fm>
2021-02-09 12:31:42 +0200

    Add delayed query send service. Send process runs in separate thread, but can only be one active such thread. Send implemented by 10 queries per thread. Also newest queries scheduled to send, and sent process will be continuing until all queries are sent (thread will start next thread until all queries are sent)

--- src/main/java/ua/com/tracktor/combine/data/QueryRepository.java
+++ src/main/java/ua/com/tracktor/combine/data/QueryRepository.java
@@ -3,5 +3,8 @@ package ua.com.tracktor.combine.data;
 import org.springframework.data.repository.CrudRepository;
 import ua.com.tracktor.combine.entity.Query;
 
+import java.util.List;
+
 public interface QueryRepository extends CrudRepository<Query, Long> {
+    List<Query> findTop10ByOrderByIdDesc();
 }
--- src/main/java/ua/com/tracktor/combine/service/QueryService.java
+++ src/main/java/ua/com/tracktor/combine/service/QueryService.java
@@ -1,14 +1,84 @@
 package ua.com.tracktor.combine.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.*;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
 import ua.com.tracktor.combine.data.QueryRepository;
 import ua.com.tracktor.combine.entity.Query;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.*;
 
 @Service
 public class QueryService {
     @Autowired
     QueryRepository queryRepository;
+
+    @Autowired
+    Environment environment;
+
+    private static final ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(0, 1,
+            0L, TimeUnit.SECONDS, new ArrayBlockingQueue<>(2));
+
+    private class ProcessDelayedQueriesTask implements Runnable, AutoCloseable {
+        private volatile boolean interruptExecution = false;
+
+        @Override
+        public void run() {
+            List<Query> queriesForProcess = queryRepository.findTop10ByOrderByIdDesc();
+            if (!queriesForProcess.isEmpty()) {
+                queriesForProcess.forEach(this::processQuery);
+                threadPoolExecutor.execute(new ProcessDelayedQueriesTask());
+            }
+        }
+
+        @Override
+        public void close() throws Exception {
+            interruptExecution = true;
+        }
+
+        private void processQuery(Query query) {
+            if (!interruptExecution) {
+                String relQuery = "/hs/viber/event/" + query.getAccount();
+                try {
+                    String scheme = environment.getProperty("viber-service.server.scheme");
+                    String address = environment.getProperty("viber-service.server.address");
+                    String path = environment.getProperty("viber-service.server.path") + query.getAccount();
+                    int port = Integer.parseInt(Objects.requireNonNull(environment.getProperty("viber-service.server.port")));
+
+                    URI uri = new URI(scheme, null, address, port, path, null, null);
+
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.add("X-Viber-Content-Signature", query.getSignature());
+                    headers.add("X-Delayed-Content", "true");
+
+                    HttpEntity<String> httpEntity = new HttpEntity<>(query.getRequestBody(), headers);
+                    RestTemplate restTemplate = new RestTemplate();
+                    restTemplate.getMessageConverters()
+                            .add(0, new StringHttpMessageConverter(StandardCharsets.UTF_8)); // for correct cyrillic symbols in string body
+
+                    ResponseEntity<String> responseEntity = restTemplate.exchange(uri, HttpMethod.POST, httpEntity, String.class);
+                    if (responseEntity.getStatusCode() == HttpStatus.OK) {
+                        queryRepository.delete(query);
+                    }
+
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                } catch (HttpClientErrorException | HttpServerErrorException e) {
+                    String responseBody = e.getResponseBodyAsString();
+                }
+            }
+        }
+    }
 
     public Query saveQuery(String signature, String account, String body) {
         Query query = new Query();
@@ -17,5 +87,13 @@ public class QueryService {
         query.setRequestBody(body);
 
         return queryRepository.save(query);
+    }
+
+    public void runDelayedQueryProcessing() {
+        if (!threadPoolExecutor.isShutdown()) {
+            if (threadPoolExecutor.getQueue().remainingCapacity() > 0) {
+                threadPoolExecutor.execute(new ProcessDelayedQueriesTask());
+            }
+        }
     }
 }
