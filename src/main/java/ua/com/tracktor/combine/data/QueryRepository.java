52f2c5a91052822b573b1af14e7ff3b34897dea4
Stepan <sters@ua.fm>
2021-02-09 15:02:28 +0200

    Add "Not processed due error" flag because we can get looping in situation when there more then 10 queries with error. In such case we cycled on processing that error queries all the time. Queries with switched that flag excludes from processing, but does not delete from database for further analising and processing

--- src/main/java/ua/com/tracktor/combine/data/QueryRepository.java
+++ src/main/java/ua/com/tracktor/combine/data/QueryRepository.java
@@ -6,5 +6,5 @@ import ua.com.tracktor.combine.entity.Query;
 import java.util.List;
 
 public interface QueryRepository extends CrudRepository<Query, Long> {
-    List<Query> findTop10ByOrderByIdDesc();
+    List<Query> findTop10ByProcessingErrorFalseOrderByIdDesc();
 }
