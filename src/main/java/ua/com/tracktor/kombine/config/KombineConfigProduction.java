60ce1804b22de10fe7ed597cd6764ad59cc35e02
Stepan <sters@ua.fm>
2021-04-17 14:57:57 +0300

    Make property source files profile-dependent

--- src/main/java/ua/com/tracktor/kombine/config/KombineConfigProduction.java
+++ src/main/java/ua/com/tracktor/kombine/config/KombineConfigProduction.java
@@ -1,12 +1,12 @@
 package ua.com.tracktor.kombine.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-//@PropertySource("classpath:application-dev.properties")
-//@PropertySource("classpath:credentials-dev.properties")
+@Profile("production")
 @PropertySource("classpath:application-prod.properties")
 @PropertySource("classpath:credentials-prod.properties")
-public class KombineConfig {
+public class KombineConfigProduction {
 }
