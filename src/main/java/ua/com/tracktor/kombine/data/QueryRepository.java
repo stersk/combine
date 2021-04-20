2ded7054de201ff18bd68fc3927c5ac54062365a
Stepan <sters@ua.fm>
2021-04-20 17:10:31 +0300

    Refactor: make successfully processed queries deleting by schedule (every day at 0:00:00). Delete queries older then x days, which is configurable

--- src/main/java/ua/com/tracktor/kombine/data/QueryRepository.java
+++ src/main/java/ua/com/tracktor/kombine/data/QueryRepository.java
@@ -3,8 +3,11 @@ package ua.com.tracktor.kombine.data;
 import org.springframework.data.repository.CrudRepository;
 import ua.com.tracktor.kombine.entity.Query;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 public interface QueryRepository extends CrudRepository<Query, Long> {
-    List<Query> findTop10ByProcessingErrorFalseOrderByIdDesc();
+    List<Query> findTop10ByProcessingResultCodeOrderByRequestDateDesc(int notProcessedCode);
+    List<Query> findTop10ByProcessingResultCodeNotAndRetryTrueAndProcessingDateBeforeOrderByRequestDateDesc(int notProcessedCode, Timestamp lastErrorDateAfter);
+    List<Query> findByProcessingResultCodeAndProcessingDateBefore(int processedCode, Timestamp startProcessedDate);
 }
